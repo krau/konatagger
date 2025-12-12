6c513f0db15f9ca6605894bf2b3ef18221abfd0d
krau <71133316+krau@users.noreply.github.com>
2025-12-12 17:49:26 +0800

    fix: move server initialization into goroutine for better structure

--- main.go
+++ main.go
@@ -17,27 +17,27 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 	slog.Info("Starting KonaTagger")
-
-	ort.SetSharedLibraryPath(onnx.LibPath())
-	if err := ort.InitializeEnvironment(); err != nil {
-		slog.Error("Failed to initialize ONNX Runtime environment", slog.String("error", err.Error()))
-		return
-	}
-	defer ort.DestroyEnvironment()
-
-	if err := server.Init(ctx); err != nil {
-		slog.Error("Failed to initialize server", slog.String("error", err.Error()))
-		return
-	}
-
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-	r.POST("/predict", server.PredictHandler)
-	r.GET("/health", server.HealthHandler)
-
-	addr := config.C().Host + ":" + config.C().Port
-	slog.Info("Listening on", slog.String("address", addr))
 	go func() {
+		ort.SetSharedLibraryPath(onnx.LibPath())
+		if err := ort.InitializeEnvironment(); err != nil {
+			slog.Error("Failed to initialize ONNX Runtime environment", slog.String("error", err.Error()))
+			return
+		}
+		defer ort.DestroyEnvironment()
+
+		if err := server.Init(ctx); err != nil {
+			slog.Error("Failed to initialize server", slog.String("error", err.Error()))
+			return
+		}
+
+		gin.SetMode(gin.ReleaseMode)
+		r := gin.Default()
+		r.POST("/predict", server.PredictHandler)
+		r.GET("/health", server.HealthHandler)
+
+		addr := config.C().Host + ":" + config.C().Port
+		slog.Info("Listening on", slog.String("address", addr))
+
 		if err := r.Run(addr); err != nil {
 			slog.Error("Server error", slog.String("error", err.Error()))
 			cancel()
